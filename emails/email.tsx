12cb53736f1e4c363e9bd48fa57f4cd8257f0786
Sam Fletcher <sfletchertx@gmail.com>
2024-03-30 13:32:45 -0500

    Init

---
+++ emails/email.tsx
@@ -0,0 +1,28 @@
+import { Tailwind, Button, Heading } from "@react-email/components";
+import React from "react";
+
+const Email = () => {
+  return (
+    <Tailwind
+      config={{
+        theme: {
+          extend: {
+            colors: {
+              brand: "#007291",
+            },
+          },
+        },
+      }}
+    >
+      <Heading>Test</Heading>
+      <Button
+        href="https://example.com"
+        className="bg-brand px-3 py-2 font-medium leading-4 text-white"
+      >
+        Click me
+      </Button>
+    </Tailwind>
+  );
+};
+
+export default Email;
